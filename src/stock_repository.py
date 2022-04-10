c716496b1a50b49b746c7831f3e5e1794af0157a
Janne Kärki <janne.karki@helsinki.fi>
2022-04-10 16:26:55 +0300

    osakkeen myynti toimimaan tietokantaan

--- src/stock_repository.py
+++ src/stock_repository.py
@@ -28,17 +28,17 @@ class StockRepository:
         #tähän UPDATE toiminto
         
 
-    def remove_stock_from_portfolio(self,stock,amount):
+    def remove_stock_from_portfolio(self,user,stock,price,amount):
         stocks_database = self.connection.cursor()
-        stocks_database.execute("SELECT avg_price,amount FROM Stocks WHERE content=?", [stock])
+        stocks_database.execute("SELECT avg_price,amount FROM Stocks WHERE user = ? and content=?", [user,stock])
         data=stocks_database.fetchall()
         if len(data) > 0:            
             old_amount = data[0][1]
             if amount == old_amount:
-                stocks_database.execute("DELETE FROM Stocks (content) values (?)", [stock])
+                stocks_database.execute("DELETE * FROM Stocks WHERE user = ? and content = ?", [user, stock])
             if amount < old_amount:
                 new_amount= data[0][1]-amount
-                stocks_database.execute("UPDATE Stocks SET amount = ? where content = ?", [new_amount, stock])
+                stocks_database.execute("UPDATE Stocks SET amount = ? where user = ? and content = ?", [new_amount,user, stock])
         else:
              print("not in portfolio") # jos 0 niin ei ole tietokannassa
         
