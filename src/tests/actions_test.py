7d294373d67782f0117b372c65056b03cdb7c8f5
Janne Kärki <janne.karki@helsinki.fi>
2022-04-11 14:46:13 +0300

    action-luokna testien korjailuja ja poistetun investor-luokan testit poistettu

--- src/tests/actions_test.py
+++ src/tests/actions_test.py
@@ -1,22 +1,25 @@
 import unittest
-from investor import Investor
+from user import User
 from actions import Actions
+from user_repository import (user_repository as default_user_repository)
+from stock_repository import (stock_repository as default_stock_repository)
+
 
 class TestActions(unittest.TestCase):
     def setUp(self):
-        self.investor = Investor("ABC", 10000)
-        self.actions = Actions(self.investor)
+        self.actions = Actions()
+        self.user = User("Erkki", "1234", 10000)
 
     def test_buy_stock_capital_decreases(self):
         self.actions.buy_stock('NOK', 1)
-        self.assertLess(self.investor.capital, 10000)
+        self.assertLess(self.actions.show_capital, 10000)
 
     def test_bought_stock_is_in_portfolio(self):
         self.actions.buy_stock('NOK', 1)
-        found = 'NOK' in self.investor.portfolio
+        found = 'NOK' in self.actions.get_portfolio()
         self.assertEqual(found, True)
 
     def test_stock_amount_is_correct_in_portfolio(self):
         self.actions.buy_stock('NOK',1)
-        amount = self.investor.portfolio['NOK'][0]
+        amount = self.actions.get_portfolio['NOK'][0]
         self.assertEqual(amount,1)
