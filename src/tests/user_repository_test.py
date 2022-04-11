da937db7d30af9e12c644d4814b89693f784825a
Janne Kärki <janne.karki@helsinki.fi>
2022-04-11 15:44:29 +0300

    uuden käyttäjän testi lisätty

---
+++ src/tests/user_repository_test.py
@@ -0,0 +1,15 @@
+import unittest
+from user_repository import user_repository
+from user import User
+
+class TestUserRepository(unittest.TestCase):
+    def setUp(self):
+        user_repository.delete_all()
+        self.user_erkki = User("Erkki","1234", 10000)
+
+    def test_new_user(self):
+        user_repository.new_user(self.user_erkki)
+        user = user_repository.find_user(self.user_erkki.username)
+        
+        self.assertEqual(user[0], self.user_erkki.username)
+        self.assertEqual(user[2], 10000)
