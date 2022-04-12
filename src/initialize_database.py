75613fd3218acfc62e2112a3e6e22c1c5c8ce0df
Janne Kärki <janne.karki@helsinki.fi>
2022-04-12 10:52:27 +0300

    pylint asennettu

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -15,8 +15,16 @@ def drop_tables(parameter):
 def create_tables(connection):
 
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE Stocks(user text, content text, avg_price float(24), amount integer)")
-    cursor.execute("CREATE TABLE Users(username text primary key, password text, capital float(24))")
+    cursor.execute("""CREATE TABLE Stocks(
+                    user text, 
+                    content text, 
+                    avg_price float(24), 
+                    amount integer)""")
+
+    cursor.execute("""CREATE TABLE Users(
+                    username text primary key, 
+                    password text, 
+                    capital float(24))""")
     connection.commit()
 
 
