d7c6d4e6325714871ae57c59c5e2a1e56c78fae9
Janne Kärki <janne.karki@helsinki.fi>
2022-04-16 17:27:57 +0300

    isolation lev none toimii

--- src/database_connection.py
+++ src/database_connection.py
@@ -5,7 +5,7 @@ from config import DATABASE_FILE_PATH
 dirname = os.path.dirname(__file__)
 
 connection = sqlite3.connect(DATABASE_FILE_PATH)
-connection.row_factory = sqlite3.Row
+connection.isolation_level = None
 
 
 
