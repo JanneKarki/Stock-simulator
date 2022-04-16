74f2a41a7230027439924702875fbf65052c8bbf
Janne Kärki <janne.karki@helsinki.fi>
2022-04-16 17:58:20 +0300

    ikkuna

--- src/ui/tkinter_ui.py
+++ src/ui/tkinter_ui.py
@@ -1,7 +1,21 @@
-from tkinter import Tk
+from tkinter import Tk, ttk
+
+
+class UI:
+    def __init__(self,root):
+        self.__root = root
+
+    def start (self):
+        label = ttk.Label(master=self.__root, text = "Hello world!")
+
+        label.pack()
 
 window = Tk()
 
 window.title("TkInter example")
+
+ui = UI(window)
+
+ui.start()
 
 window.mainloop()
