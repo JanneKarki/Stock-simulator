a438fc048e4a3b22b19bf40fd12523308b5b14ee
Janne Kärki <janne.karki@helsinki.fi>
2022-04-17 12:22:29 +0300

    login example

--- src/ui/tkinter_ui.py
+++ src/ui/tkinter_ui.py
@@ -7,17 +7,27 @@ class UI:
         self.__root = root
 
     def start (self):
-        label = ttk.Label(master=self.__root, text = "Hello world!")
-        button = ttk.Label(master=self.__root, text="Button")
-        entry = ttk.Entry(master=self.__root)
-        checkbutton = ttk.Checkbutton(master=self.__root, text="Check button")
-        radiobutton = ttk.Radiobutton(master=self.__root, text="Radio button")
+        heading_label = ttk.Label(master=self.__root, text = "Login")
+        
+        username_label = ttk.Label(master=self.__root, text="Username")
+        username_entry = ttk.Entry(master=self.__root)
 
-        label.pack(side=constants.LEFT)
-        button.pack(side=constants.LEFT)
-        entry.pack()
-        checkbutton.pack()
-        radiobutton.pack()
+        password_label = ttk.Label(master=self.__root, text="Password")
+        password_entry = ttk.Entry(master=self.__root)
+
+        button = ttk.Button(master=self.__root, text="Submit")
+        
+        heading_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+
+        username_label.grid(row=1, column=0, padx=5, pady=5)
+        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+
+        password_label.grid(row=2, column=0, padx=5, pady=5)
+        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+
+        button.grid(row=3,column=0,columnspan=2, padx=5, pady=5)
+
+        self.__root.grid_columnconfigure(1, weight=1, minsize=250)
 
 window = Tk()
 
